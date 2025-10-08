4bd1696d0f4c324083b705403541bb05976f60ba
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-10-08 10:30:44 +0100

    Competer l'integration du blog

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -24,7 +24,7 @@
                     <td>{{ $blog->title }}</td>
                     <td>{{ $blog->blogType->name ?? 'N/A' }}</td>
                     <td>{{ optional($blog->author)->name ?? 'N/A' }}</td>
-                    <td>{{ $blog->description }}</td>
+                    <td>{{ $blog->content }}</td>
                     <td>{{ optional($blog->tags)->count() ? $blog->tags->pluck('name')->implode(', ') : 'No tags' }}</td>
                     <td>{{ \Carbon\Carbon::parse($blog->publication_date)->format('Y-m-d H:i') }}</td>
                     <td>
@@ -179,17 +179,6 @@
                     </button>
                 </form>
             </div>
-
-            @auth
-            <div class="mt-8">
-                <a href="{{ route('blogs.create') }}" class="inline-flex items-center gap-2 bg-white text-green-600 font-bold px-8 py-4 rounded-full hover:bg-green-50 transform hover:scale-105 transition-all duration-300 shadow-xl">
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                    </svg>
-                    Créer un Article
-                </a>
-            </div>
-            @endauth
         </div>
     </section>
 
@@ -258,26 +247,6 @@
                                                 </p>
                                             </div>
                                         </div>
-
-                                        {{-- Actions (visible au hover) --}}
-                                        @auth
-                                        <div class="flex gap-2 action-btn">
-                                            <a href="{{ route('blogs.edit', $blog) }}" class="p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300" title="Modifier">
-                                                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                </svg>
-                                            </a>
-                                            <form action="{{ route('blogs.destroy', $blog) }}" method="POST" onsubmit="return confirm('Supprimer cet article ?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="p-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors duration-300" title="Supprimer">
-                                                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                        </div>
-                                        @endauth
                                     </div>
                                 </div>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,9 @@ Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.crea
 // Routes publiques
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
 Route::get('/blogs/{blog}', [BlogController::class, 'show'])->name('blogs.show');
+Route::get('/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
+Route::put('/blogs/{blog}', [BlogController::class, 'update'])->name('blogs.update');
+Route::delete('/blogs/{blog}', [BlogController::class, 'destroy'])->name('blogs.destroy');
 
 
 
@@ -52,9 +55,6 @@ Route::middleware('auth')->group(function () {
     })->name('chatbot');
     Route::post('/chatbot', [ChatbotController::class, 'getResponse'])->name('chatbot.response');
     Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
-    Route::get('/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
-    Route::put('/blogs/{blog}', [BlogController::class, 'update'])->name('blogs.update');
-    Route::delete('/blogs/{blog}', [BlogController::class, 'destroy'])->name('blogs.destroy');
     
 });
 
